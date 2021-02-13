b761dcbd293041e53f61d010ee3d2b4a717aff92
Eric Ribeiro <ericribeiro@outlook.com.br>
2021-02-13 03:45:21 -0800

    added example

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,8 @@
 import { Probot } from "probot";
 
 export = (app: Probot) => {
-  app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
-    });
-    await context.octokit.issues.createComment(issueComment);
+  app.on("pull_request.opened", async (context: any) => {
+    context.log(`Bot's Name: ${context.payload["originalBotName"]}`);
+    context.log(`Bot's Comment: ${context.payload["originalBotComment"]}`);
   });
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 };
