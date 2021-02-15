75c420af4066808b6fb63f1e37082cb8afb18388
Eric Ribeiro <ericribeiro@outlook.com.br>
2021-02-15 09:46:02 -0800

    updated bot comsumption example

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import { Probot } from "probot";
 
+import type { MetabotWebhookPayloadPullRequest } from 'metabot-utils'
+
 export = (app: Probot) => {
   app.on("pull_request.opened", async (context: any) => {
-    context.log(`Bot's Name: ${context.payload["originalBotName"]}`);
-    context.log(`Bot's Comment: ${context.payload["originalBotComment"]}`);
+    const payload: MetabotWebhookPayloadPullRequest = context.payload;
+
+    context.log(`Bot's Name: ${payload.botName}`);
+    context.log(`Bot's Comment: ${payload.botComment}`);
   });
 };
