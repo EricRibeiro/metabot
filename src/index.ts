3d75bb3b3488d92f6c0972350daad5f0b3c4c55d
Ronan Felipe <ronan.nascimento@sga.pucminas.br>
2021-02-16 01:25:38 -0300

    heroku file

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 import { Probot } from "probot";
+import type { MetabotWebhookPayloadPullRequest } from 'metabot-utils';
 
 export = (app: Probot) => {
-  app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
-    });
-    await context.octokit.issues.createComment(issueComment);
+  app.on("pull_request.opened", async (context: any) => {
+    // const issueComment = context.issue({
+    //   body: context.payload["originalBotComment"],
+    // });
+    // await context.octokit.issues.createComment(issueComment);
+    
+    const payload: MetabotWebhookPayloadPullRequest = context.payload;
+
+    context.log(`Bot's Name: ${payload.botName}`);
+    context.log(`Bot's Comment: ${payload.botComment}`);
+    
+    console.log();
   });
   // For more information on building apps:
   // https://probot.github.io/docs/
