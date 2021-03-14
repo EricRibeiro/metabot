491c4eaa41511c8a89c83b78e53ae07970d7f749
Eric Ribeiro <ericribeiro@outlook.com.br>
2021-03-13 23:04:26 -0800

    changed comment format

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,3 +1,58 @@
 export function sleep(milliseconds: number): Promise<null> {
-    return new Promise(resolve => setTimeout(resolve, milliseconds));
+  return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
+
+export function getGitHubComment(prOwner: string, documents: any): string {
+  const commentsPerLabelCount = documents.length;
+  const commentsPerLabelPerBot = {}
+
+  documents.forEach((curr) => {
+    commentsPerLabelPerBot[curr.label] = commentsPerLabelPerBot[curr.label]
+      ? commentsPerLabelPerBot[curr.label]
+      : {};
+
+    commentsPerLabelPerBot[curr.label][curr.comment.user.login] = commentsPerLabelPerBot[curr.label][curr.comment.user.login]
+      ? commentsPerLabelPerBot[curr.label][curr.comment.user.login]
+      : [];
+
+    commentsPerLabelPerBot[curr.label][curr.comment.user.login].push(curr.comment.body);
+  });
+
+  let body = `\
+        Hi @${prOwner}
+  
+        You have ${commentsPerLabelCount} ${commentsPerLabelCount === 1 ? "comment" : "comments"}, made by bots installed on this repo, regarding this pull request. \
+        Check them below:\n`;
+
+  for (const [label, commentsPerBot] of (<any>Object).entries(commentsPerLabelPerBot)) {
+    const amountOfComments = (<any>Object)
+      .values(commentsPerBot)
+      .reduce((acc, curr) => { return acc + curr.length }, 0);
+
+    body += `\
+            <details>
+            <summary>There ${amountOfComments === 1 ? "is" : "are"} <b>${amountOfComments}</b> ${amountOfComments === 1 ? "comment" : "comments"} with the label <b>${label}</b></summary>
+            <br /> \n\n`;
+
+    for (const [bot, comments] of (<any>Object).entries(commentsPerBot)) {
+      body += `\
+        <ul>
+        <li>
+        <details>
+        <summary>${comments.length} ${comments.length === 1 ? "comment" : "comments"} made by <i>${bot}</i></summary>
+        <br />\n\n`;
+
+      comments.forEach(comment => body += `${comment}\n\n`);
+
+      body += `\
+        </details>
+        </li>
+        </ul>\n\n`;
+    }
+
+    body += `</details>`;
+  }
+
+  // removing empty tab spaces to have a valid markdown.
+  return body.replace(/  /g, "")
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Probot } from "probot";
-import { sleep } from './helpers/utils';
+import { getGitHubComment, sleep } from './helpers/utils';
 import { fetchBotsComments, saveBotComment } from './bot';
 
 export = (app: Probot) => {
@@ -7,46 +7,35 @@ export = (app: Probot) => {
 
   app.on("pull_request.opened", async (context) => {
     // waiting for all bots' comments to be posted, saved on database and deleted.
-    await sleep(6000);
+    await sleep(10000);
 
-    const { documents, error } = await fetchBotsComments(context.payload.repository.owner.login, context.payload.repository.name, context.payload.number, connString);
+    const { documents, error } = await fetchBotsComments(
+      context.payload.repository.owner.login,
+      context.payload.repository.name,
+      context.payload.number,
+      connString
+    );
+
     if (error) { throw error; }
 
-    const labelFrequency: Map<string, number> = documents.reduce((acc, curr) => acc.set(curr.label, (acc.get(curr.label) || 0) + 1), new Map());
-    const commentsPerLabelCount = [...labelFrequency.values()].reduce((acc, curr) => acc + curr);
-
-    const owner = context.payload.repository.owner.login;
-    const repo = context.payload.repository.name;
-    const issueNumber = context.payload.number;
-    const installationId = context.payload.installation?.id;
     const prOwner = context.payload.pull_request.user.login;
-
-    let body = `Hi @${prOwner}.\n\nYou have ${commentsPerLabelCount} ${commentsPerLabelCount === 1 ? "comment" : "comments"}, made by bots installed on this repo, regarding this pull request. If you want me to show them, please use the links below:\n`;
-
-    for (let label of labelFrequency.keys()) {
-      const frequency = labelFrequency.get(label);
-      body += `- There ${frequency === 1 ? "is" : "are"} ${frequency} ${frequency === 1 ? "comment" : "comments"} with the label **${label}**. `
-      body += `[Show me](https://metabot-express.herokuapp.com/comments/?owner=${owner}&repo=${repo}&issueNumber=${issueNumber}&label=${label}&installationId=${installationId}).\n`
-    }
-
-    body += `- [Show me all](https://metabot-express.herokuapp.com/comments/?owner=${owner}&repo=${repo}&issueNumber=${issueNumber}&label=all&installationId=${installationId}).\n\n`
-    body += `Let me know what you think of this comment by reacting with 👍 or 👎.`
-
+    const body = getGitHubComment(prOwner, documents);
     const issueComment = context.issue({ body });
+
     await context.octokit.issues.createComment(issueComment);
   });
 
   app.on("issue_comment.created", async (context) => {
     if (context.isBot && context.payload.comment.user.login !== "metabot-puc[bot]") {
       const { error } = await saveBotComment(
-        context.payload.repository.owner.login, 
-        context.payload.repository.name, 
-        context.payload.issue.number, 
-        context.payload.comment, 
-        context.payload.comment.user.login, 
+        context.payload.repository.owner.login,
+        context.payload.repository.name,
+        context.payload.issue.number,
+        context.payload.comment,
+        context.payload.comment.user.login,
         connString
       );
-      
+
       if (error) { throw error; }
 
       await context.octokit.issues.deleteComment({
