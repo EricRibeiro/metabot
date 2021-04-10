928f31da777d9f8661c882bde476f5722cd3d22b
Eric Ribeiro <ericribeiro@outlook.com.br>
2021-04-10 13:55:17 -0700

    fixed time comparison bug

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,37 @@
 import { Probot } from "probot";
-// import { sleep } from './helpers/utils';
+import { sleep } from './helpers/utils';
 import { buildGitHubComment, fetchBotsComments, saveBotComment, labelResolver } from './bot';
+import { DateTime } from 'luxon'
 
 import type { BotsConfig } from "./helpers/types"
 
-// const SLEEP_TIME_MILLISECONDS = 10000;
+const SLEEP_TIME_MILLISECONDS = 10000;
 
 export = (app: Probot) => {
   const connString = process.env.MONGO_CONN_STRING!;
 
-  // app.on("pull_request.opened", async (context) => {
-  //   // waiting for all bots' comments to be posted, saved on database and deleted.
-  //   await sleep(SLEEP_TIME_MILLISECONDS);
-  //   const config = <BotsConfig>(await context.config("config.yml"));
+  app.on("pull_request.opened", async (context) => {
+    // waiting for all bots' comments to be posted, saved on database and deleted.
+    await sleep(SLEEP_TIME_MILLISECONDS);
+    const config = <BotsConfig>(await context.config("config.yml"));
 
-  //   const { documents, error } = await fetchBotsComments(
-  //     context.payload.repository.owner.login,
-  //     context.payload.repository.name,
-  //     context.payload.number,
-  //     connString
-  //   );
+    const { documents, error } = await fetchBotsComments(
+      context.payload.repository.owner.login,
+      context.payload.repository.name,
+      context.payload.number,
+      connString
+    );
 
-  //   if (error) throw error;
+    if (error) throw error;
 
-  //   if (documents.length > 0) {
-  //     const prOwner = context.payload.pull_request.user.login;
-  //     const body = buildGitHubComment(prOwner, documents, config.metabotBotsToWaitForComment);
-  //     const issueComment = context.issue({ body });
+    if (documents.length > 0) {
+      const prOwner = context.payload.pull_request.user.login;
+      const body = buildGitHubComment(prOwner, documents, config.metabotBotsToWaitForComment);
+      const issueComment = context.issue({ body });
 
-  //     await context.octokit.issues.createComment(issueComment);
-  //   }
-  // });
+      await context.octokit.issues.createComment(issueComment);
+    }
+  });
 
   app.on("issue_comment.created", async (context) => {
     if (context.isBot && context.payload.comment.user.login !== "metabot-puc[bot]") {
@@ -62,11 +63,9 @@ export = (app: Probot) => {
 
       const metabotComment = comments.data.find(comment => comment.user?.login === "metabot-puc[bot]")
 
-      // const issueCreationTime = new Date(context.payload.issue.created_at);
-      // const currentTime = new Date();
-
-      // const setDateOffset = (date: Date, offset: number): Date => new Date(date.setSeconds(date.getSeconds() + offset));
-      // const issueCreationTimeWithOffset = setDateOffset(issueCreationTime, SLEEP_TIME_MILLISECONDS);
+      const issueCreationTimeWithOffset = DateTime.fromISO(context.payload.issue.created_at)
+        .plus({ milliseconds: SLEEP_TIME_MILLISECONDS });
+      const currentTime = DateTime.now();
 
       if (metabotComment) {
         const { documents, error } = await fetchBotsComments(
@@ -86,7 +85,7 @@ export = (app: Probot) => {
           body
         });
 
-      } else {
+      } else if (currentTime > issueCreationTimeWithOffset) {
         const { documents, error } = await fetchBotsComments(
           context.payload.repository.owner.login,
           context.payload.repository.name,
