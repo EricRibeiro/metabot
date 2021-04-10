ffd9bf2e4194b04b57ebef53749ad7f2911fae8d
Eric Ribeiro <ericribeiro@outlook.com.br>
2021-04-10 11:10:55 -0700

    removed pull request opening listener

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,36 @@
 import { Probot } from "probot";
-import { buildGitHubComment, sleep } from './helpers/utils';
-import { fetchBotsComments, saveBotComment, labelResolver } from './bot';
+// import { sleep } from './helpers/utils';
+import { buildGitHubComment, fetchBotsComments, saveBotComment, labelResolver } from './bot';
 
 import type { BotsConfig } from "./helpers/types"
+
+// const SLEEP_TIME_MILLISECONDS = 10000;
 
 export = (app: Probot) => {
   const connString = process.env.MONGO_CONN_STRING!;
 
-  app.on("pull_request.opened", async (context) => {
-    // waiting for all bots' comments to be posted, saved on database and deleted.
-    await sleep(10000);
-    const config = <BotsConfig>(await context.config("config.yml"));
+  // app.on("pull_request.opened", async (context) => {
+  //   // waiting for all bots' comments to be posted, saved on database and deleted.
+  //   await sleep(SLEEP_TIME_MILLISECONDS);
+  //   const config = <BotsConfig>(await context.config("config.yml"));
 
-    const { documents, error } = await fetchBotsComments(
-      context.payload.repository.owner.login,
-      context.payload.repository.name,
-      context.payload.number,
-      connString
-    );
+  //   const { documents, error } = await fetchBotsComments(
+  //     context.payload.repository.owner.login,
+  //     context.payload.repository.name,
+  //     context.payload.number,
+  //     connString
+  //   );
 
-    if (error) throw error;
-    
-    if (documents.length > 0) {
-      const prOwner = context.payload.pull_request.user.login;
-      const body = buildGitHubComment(prOwner, documents, config.metabotBotsToWaitForComment);
-      const issueComment = context.issue({ body });
-      
-      await context.octokit.issues.createComment(issueComment);
-    }
-  });
+  //   if (error) throw error;
+
+  //   if (documents.length > 0) {
+  //     const prOwner = context.payload.pull_request.user.login;
+  //     const body = buildGitHubComment(prOwner, documents, config.metabotBotsToWaitForComment);
+  //     const issueComment = context.issue({ body });
+
+  //     await context.octokit.issues.createComment(issueComment);
+  //   }
+  // });
 
   app.on("issue_comment.created", async (context) => {
     if (context.isBot && context.payload.comment.user.login !== "metabot-puc[bot]") {
@@ -60,6 +62,12 @@ export = (app: Probot) => {
 
       const metabotComment = comments.data.find(comment => comment.user?.login === "metabot-puc[bot]")
 
+      // const issueCreationTime = new Date(context.payload.issue.created_at);
+      // const currentTime = new Date();
+
+      // const setDateOffset = (date: Date, offset: number): Date => new Date(date.setSeconds(date.getSeconds() + offset));
+      // const issueCreationTimeWithOffset = setDateOffset(issueCreationTime, SLEEP_TIME_MILLISECONDS);
+
       if (metabotComment) {
         const { documents, error } = await fetchBotsComments(
           context.payload.repository.owner.login,
@@ -77,6 +85,24 @@ export = (app: Probot) => {
           comment_id: metabotComment.id,
           body
         });
+
+      } else {
+        const { documents, error } = await fetchBotsComments(
+          context.payload.repository.owner.login,
+          context.payload.repository.name,
+          context.payload.issue.number,
+          connString
+        );
+
+        if (error) throw error;
+
+        if (documents.length > 0) {
+          const prOwner = context.payload.repository.owner.login;
+          const body = buildGitHubComment(prOwner, documents, config.metabotBotsToWaitForComment);
+          const issueComment = context.issue({ body });
+
+          await context.octokit.issues.createComment(issueComment);
+        }
       }
     } else {
       //user comment
